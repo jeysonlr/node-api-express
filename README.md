d71ff2165db9bb14f2cfc867ee9bfe28f09f2b59
jeysonlr <jeysonlr@gmail.com>
2020-06-21 13:47:10 -0300

    Adicionada documentação Swagger

--- README.md
+++ README.md
@@ -11,5 +11,8 @@
     node src/index
 
     a aplicação iniciara em localhost:3000
+
+# documentação
+    a documentação podera ser encontrado no endereço localhost:3000/doc
     
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const YAML = require('yamljs')
+const swaggerDoc = YAML.load('./swagger.yaml')
+const swaggerUi = require('swagger-ui-express')
+
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 require('./app/controllers/index')(app)
 
